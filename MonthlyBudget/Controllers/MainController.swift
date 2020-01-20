473e3ea2a579b2a326ce51c0e8bfe812c906ebc7
andersshv@gmail.com <andersshv@gmail.com>
2020-01-20 23:21:21 +0100

    ok

---
+++ MonthlyBudget/Controllers/MainController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  MonthlyBudget
+//
+//  Created by Anders Strand-Holm Vinther on 14/01/2020.
+//  Copyright © 2020 ashvpps. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    @IBOutlet weak var AmountPerMonthLabel: UILabel!
+    @IBOutlet weak var AmountPerMonthTextField: UITextField!
+    
+    @IBOutlet weak var AvailablePerDayLabel: UILabel!
+        
+    @IBOutlet weak var NewPostingDescriptionTextField: UITextField!    
+    @IBOutlet weak var NewPostingAmountTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateGUI()
+    }
+
+    @IBAction func newAmountPerMonthSubmitButtonClicked(_ sender: Any) {
+        if let amount = AmountPerMonthTextField.text {
+            AmountPerMonthTextField.text = ""
+            saveAmountPerMonth(amount: Int(amount)!)
+        }
+        updateGUI()
+    }
+    
+    func updateGUI() {
+        let amountPerMonth = getAmountPerMonth()
+        AmountPerMonthLabel.text = String(amountPerMonth)
+        
+        let usedSoFar = getAmountUsedSoFar()
+        let daysLeft = getDaysLeftInMonth()
+        
+        let moneyLeft = amountPerMonth - usedSoFar
+        let availablePerDay = Int(moneyLeft / daysLeft)
+        AvailablePerDayLabel.text = String(availablePerDay)
+    }
+
+    @IBAction func newPostingSubmitButtonClicked(_ sender: Any) {
+        if let description = NewPostingDescriptionTextField.text, let amount = NewPostingAmountTextField.text {
+            NewPostingDescriptionTextField.text = ""
+            NewPostingAmountTextField.text = ""
+            saveNewPosting(description, Int(amount)!)
+        }
+        updateGUI()
+    }
+    
+}
+
--- MonthlyBudget/Helper.swift
+++ MonthlyBudget/Helper.swift
@@ -8,26 +8,35 @@
 
 import Foundation
 
-func getDayFromDate(date: Date) -> String {
-    let dateString = date.description
-    let dateStringSplit = dateString.split(separator: "-")
-    let daysPart = dateStringSplit[2]
+func getDaysLeftInMonth() -> Int {
+    let date = Date()
+    let year = getYear(date)
+    let month = getMonth(date)
+    let daysInMonth = getDaysInMonth(year, month);
+    let day = getDay(date)
+    return daysInMonth - Int(day)! + 1
+}
+
+func getYear(_ date: Date) -> String {
+    return String(getDateSplit(date)[0])
+}
+
+func getMonth(_ date: Date) -> String {
+    return String(getDateSplit(date)[1])
+}
+
+func getDay(_ date: Date) -> String {
+    let daysPart = getDateSplit(date)[2]
     return String(daysPart.prefix(2));
 }
 
-func getMonthFromDate(date: Date) -> String {
+func getDateSplit(_ date: Date) -> [String.SubSequence] {
     let dateString = date.description
     let dateStringSplit = dateString.split(separator: "-")
-    return String(dateStringSplit[1]);
+    return dateStringSplit
 }
 
-func getYearFromDate(date: Date) -> String {
-    let dateString = date.description
-    let dateStringSplit = dateString.split(separator: "-")
-    return String(dateStringSplit[0]);
-}
-
-func getNumberOfDaysInMonth(year: String, month: String) -> Int {
+func getDaysInMonth(_ year: String, _ month: String) -> Int {
     let dateComponents = DateComponents(year: Int(year), month: Int(month))
     let calendar = Calendar.current
     let date = calendar.date(from: dateComponents)!
@@ -37,6 +46,35 @@ func getNumberOfDaysInMonth(year: String, month: String) -> Int {
     return numDays;
 }
 
-func getUsedSoFarThisMonth() -> Int {
+/******** STORAGE *********/
+
+let amountPerMonth = "amountPerMonth"
+
+func getAmountPerMonth() -> Int {
+    if let val = getSavedValue(amountPerMonth) {
+        return val
+    }
+    return 0
+}
+
+func saveAmountPerMonth(amount: Int) {
+    UserDefaults.standard.set(amount, forKey: amountPerMonth)
+}
+
+func getAmountUsedSoFar() -> Int {
+    let date = Date()
+    let year = getYear(date)
+    let month = getMonth(date)
+    let key = year + month
+    let spendingsForMonth = getSavedValue(key)
+    
     return 1000;
+}
+
+func saveNewPosting(_ description: String, _ amount: Int) {
+    
+}
+
+func getSavedValue(_ key: String) -> Int? {
+    return UserDefaults.standard.integer(forKey: key)
 }
